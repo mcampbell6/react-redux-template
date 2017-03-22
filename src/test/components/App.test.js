3b59d74cb5b1439afb6d57f4b81e5e21ff2bbe30
mcampbell6 <11.campbell.mike@gmail.com>
2017-03-22 09:29:59 -0400

    Update App.test.js
    changed minor test naming

--- src/test/components/App.test.js
+++ src/test/components/App.test.js
@@ -2,6 +2,6 @@ import React from 'react';
 import App from '../../app/components/App';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+it('renders the App component without crashing', () => {
   shallow(<App />);
 });
